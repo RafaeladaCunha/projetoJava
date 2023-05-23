3a2389915f1fbdb88cc116891eaf74eda4f35cac
RafaelaCunha <rafaelacunha@edu.unifil.br>
2023-05-23 20:35:28 -0300

    Adicionei um arquivo java

---
+++ rafaela/src/com/company/OrdemCrescente.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class OrdemCrescente {
+    public static void main(String[] args) {
+        int numero, x =0;
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número para o tamanho do vetor");
+        numero = scanner.nextInt();
+        int [] i = new int[numero];
+
+        while(x < numero){
+
+            System.out.println(" ");
+            x = scanner.nextInt();
+
+        }
+
+
+
+
+    }
+}
+
